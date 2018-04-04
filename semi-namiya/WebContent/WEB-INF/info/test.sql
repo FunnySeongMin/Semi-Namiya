0498a304b35557dbe395f99dcd56097b1ebe4645
성민 <@192.168.0.121>
2018-04-04 15:43:32 +0900

    test.sql

---
+++ semi-namiya/WebContent/WEB-INF/info/test.sql
@@ -0,0 +1,14 @@
+INSERT INTO namiya_user (id, password, nickname , grade ) 
+VALUES('charminguk2@naver.com','123','NM욱이','a'); 
+
+INSERT INTO namiya_user (id, password, nickname , grade ) 
+VALUES('sch_kidae@naver.com','gkwlak90','NM나미야상','a'); 
+
+INSERT INTO namiya_user (id, password, nickname , grade ) 
+VALUES('rws4987@naver.com','dhkstjs2','NM해적왕','a'); 
+
+INSERT INTO namiya_user (id, password, nickname , grade ) 
+VALUES('moon-over@naver.com','123','NM편지쓰는고양이','a'); 
+
+INSERT INTO namiya_user (id, password, nickname , grade ) 
+VALUES('cloudysunny@gamil.com','123','NM고독한 뚠뚠까치','a'); 
