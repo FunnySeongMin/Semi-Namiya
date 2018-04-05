cad9ee58ff5e32a3f061b6290f6434d0cb3a674f
kosta <kosta@192.168.0.159>
2018-04-05 17:51:09 +0900

    글 삭제,수정 차단

--- semi-namiya/src/com/namiya/controller/post/UpdatePostViewController.java
+++ semi-namiya/src/com/namiya/controller/post/UpdatePostViewController.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpSession;
 import com.namiya.controller.Controller;
 import com.namiya.model.NamiyaDAO;
 import com.namiya.model.NamiyaPostVO;
+import com.namiya.model.NamiyaUserVO;
 
 public class UpdatePostViewController implements Controller {
 
@@ -15,11 +16,16 @@ public class UpdatePostViewController implements Controller {
 		//게시글 수정 form에 postVO의 내용을 넘겨주기 위한 컨트롤러
 		NamiyaPostVO postVO = null;
 		HttpSession session = request.getSession(false);
+		
 		if (session == null || session.getAttribute("userVO") == null) {
 			return "redirect:index.jsp";
 		}
+		NamiyaUserVO userVO = (NamiyaUserVO)session.getAttribute("userVO");
 		int pNo = Integer.parseInt(request.getParameter("pNo"));
 		postVO = NamiyaDAO.getInstance().readPostInfo(pNo);
+		if(!postVO.getUserVO().getId().equals(userVO.getId())) {
+			return "accesslimit.jsp";
+		}
 		request.setAttribute("postVO", postVO);
 		request.setAttribute("url", "/post/updatePost.jsp");
 		return "home.jsp";
